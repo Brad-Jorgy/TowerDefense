34c1f1943e108207e0692c2df635b87ef688cef5
Brad Jorgensen <brad@netangel.com>
2018-04-21 14:55:40 -0600

    Highscores and Options are now done and ready to be merged. still scoring system IE point for creep death, towers, etc. Options still need weapons coverage for towers. Additional files have been added for animations, particle systems and sound, they are not finished yet.

---
+++ Public/scripts/sound.js
@@ -0,0 +1,27 @@
+// MyGame.sound = (function () {
+//     'use strict';
+//
+//     tower.sell = function() {
+//         Sound.play("stuff");
+//     }
+//
+//     tower.upgrade = function() {
+//         Sound.play("stuff");
+//     }
+//
+//     creep.die = function() {
+//         Sound.play("stuff");
+//     }
+//
+//     missile.hit = function() {
+//         Sound.play("stuff");
+//     }
+//
+//     explosion.hit = function() {
+//         Sound.play("sounds/DeathFlash.flac");
+//     }
+//
+//     return{
+//
+//     }
+// }());
