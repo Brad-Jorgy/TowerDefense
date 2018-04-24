e5c0e1f512337a5ed238ae866243d707efab5df1
Brad-Jorgy <brad.taylor.jorgensen@gmail.com>
2018-04-23 18:53:27 -0600

    Update animation.js

--- Public/scripts/animation.js
+++ Public/scripts/animation.js
@@ -1,33 +1,33 @@
-// class Animation {
-//     // eventName, spriteSheet, spriteCount, spritesRows, spritesCols, pixelsWidth, pixelsHeight, timeInterval
-//     constructor(eventName, spriteSheet, spriteRows, spriteCols, pixelsWidth, pixelsHeight, frameInterval, startFrame) {
-//         this.eventID = eventName;
-//         this.sprites = spriteSheet;
-//         this.rows = spriteRows;
-//         this.columns = spriteCols;
-//         this.spriteWidth = pixelsWidth;
-//         this.spriteHeight = pixelsHeight;
-//         this.frameTime = frameInterval;
-//         this.currentFrame = startFrame;
-//         this.totalFrames = spriteRows * spriteCols;
-//     }
-//
-//     getSprite = function(index) {
-//         let row = index % this.spriteCols;
-//         let column = index / this.spriteRows;
-//
-//         const subImageX = row * this.spriteWidth;
-//         const subImageY = column * this.spriteHeight;
-//         return { x: subImageX, y:subImageY };
-//     };
-//
-//     nextFrame(ctx) {
-//
-//         this.currentFrame = this.currentFrame + 1;
-//         const { srcX, srcY } = getSprite(this.currentFrame);
-//         if (this.currentFrame < this.totalFrames) {
-//             return null;
-//         }
-//         return { sheet: this.sprites, spriteX: srcX, spriteY: srcY, pixelsY: this.spriteHeight, pixelX: this.spriteWidth }
-//     }
-// }
+class Animation {
+    // eventName, spriteSheet, spriteCount, spritesRows, spritesCols, pixelsWidth, pixelsHeight, timeInterval
+    constructor(eventName, spriteSheet, spriteRows, spriteCols, pixelsWidth, pixelsHeight, frameInterval, startFrame) {
+        this.eventID = eventName;
+        this.sprites = spriteSheet;
+        this.rows = spriteRows;
+        this.columns = spriteCols;
+        this.spriteWidth = pixelsWidth;
+        this.spriteHeight = pixelsHeight;
+        this.frameTime = frameInterval;
+        this.currentFrame = startFrame;
+        this.totalFrames = spriteRows * spriteCols;
+    }
+
+    getSprite = function(index) {
+        let row = index % this.spriteCols;
+        let column = index / this.spriteRows;
+
+        const subImageX = row * this.spriteWidth;
+        const subImageY = column * this.spriteHeight;
+        return { x: subImageX, y:subImageY };
+    };
+
+    nextFrame(ctx) {
+
+        this.currentFrame = this.currentFrame + 1;
+        const { srcX, srcY } = getSprite(this.currentFrame);
+        if (this.currentFrame < this.totalFrames) {
+            return null;
+        }
+        return { sheet: this.sprites, spriteX: srcX, spriteY: srcY, pixelsY: this.spriteHeight, pixelX: this.spriteWidth }
+    }
+}
